4c5ae4a735185b2eb26342df0bc4beb370500494
Michael Went <mjwenk@ucdavis.edu>
2009-08-05 05:00:17 +0000

    GBRK-255 	- Exception class for imports

---
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/GradebookImportException.java
@@ -0,0 +1,10 @@
+package org.sakaiproject.gradebook.gwt.sakai;
+
+public class GradebookImportException extends Exception {
+	
+	public GradebookImportException(String msg) 
+	{
+		super(msg); 
+	}
+
+}
