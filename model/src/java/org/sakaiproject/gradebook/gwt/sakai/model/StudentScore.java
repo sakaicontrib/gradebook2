60679db6b3d7fe6bd5b6f4c4b8fe7f870c84a325
Michael Went <mjwenk@ucdavis.edu>
2009-08-21 01:57:11 +0000

    GBRK-244 new Student score model object for stats

---
+++ model/src/java/org/sakaiproject/gradebook/gwt/sakai/model/StudentScore.java
@@ -0,0 +1,46 @@
+package org.sakaiproject.gradebook.gwt.sakai.model;
+
+import java.math.BigDecimal;
+
+public class StudentScore implements Comparable<StudentScore>
+{
+
+	private String userUid; 
+	private BigDecimal score;
+
+	public StudentScore() 
+	{
+		userUid = null; 
+		score = null; 
+	}
+	
+	public StudentScore(String userUid, BigDecimal score) {
+		super();
+		this.userUid = userUid;
+		this.score = score;
+	}
+
+	public String getUserUid() {
+		return userUid;
+	}
+	public void setUserUid(String userUid) {
+		this.userUid = userUid;
+	}
+	public BigDecimal getScore() {
+		return score;
+	}
+	public void setScore(BigDecimal score) {
+		this.score = score;
+	}
+
+	public int compareTo(StudentScore o) 
+	{
+		if (o != null && o.getScore() != null && getScore() != null) 
+		{
+			return getScore().compareTo(o.getScore()); 
+		}
+		return -1; 
+	} 
+	
+	
+}
