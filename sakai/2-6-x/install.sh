b1ee37e4d20163d890209a1277733a76b1326558
James Renfro <jlrenfro@ucdavid.edu>
2009-10-10 00:53:38 +0000

    GRBK-330
    Updating install scripts and gradebook patch.

--- sakai/2-6-x/install.sh
+++ sakai/2-6-x/install.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
-cd ..
-patch -p0 < gradebook2/sakai/2-6-x/sakai.patch
-cd gradebook
-patch -p0 < ../gradebook2/sakai/2-6-x/gradebook.patch
-cd ../sam
-patch -p0 < ../gradebook2/sakai/2-6-x/sam.patch
-cd ..
+
+cwd=`pwd`
+
+# Assume that we're somewhere below the base dir where sakai is installed 
+while [ "$cwd" != "/" ] 
+do 
+	if [ -f $cwd/pom.xml ] 
+	then
+		result=`grep "<name>Sakai Core Project </name>" $cwd/pom.xml`
+		if [ "$result" != "" ]
+		then
+			echo "Found Sakai base directory at $cwd . . . patching. "
+			cd $cwd
+			patch -p0 < gradebook2/sakai/2-6-x/sakai.patch
+			cd gradebook
+			patch -p0 < ../gradebook2/sakai/2-6-x/gradebook.patch
+			cd ../sam
+			patch -p0 < ../gradebook2/sakai/2-6-x/sam.patch
+
+			exit 0
+		else
+			echo $result
+		fi
+	fi
+	cwd=`dirname $cwd`
+done
+
+echo "Unable to find the Sakai root directory . . . aborting."
+exit 1
+
