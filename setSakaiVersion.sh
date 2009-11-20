421e23100aba20799ec62600e530fdc632caabad
James Renfro <jlrenfro@ucdavid.edu>
2009-11-20 01:14:51 +0000

    NOJIRA
    Updating setSakaiVersion script for help/pom.xml

--- setSakaiVersion.sh
+++ setSakaiVersion.sh
@@ -5,7 +5,7 @@ read oldVersion
 echo -n 'Enter new version: '
 read newVersion
 
-sed -i -e"s/<version>$oldVersion<\/version>/<version>$newVersion<\/version>/"  pom.xml model/pom.xml shared/pom.xml server/pom.xml api/pom.xml client/pom.xml war/pom.xml
+sed -i -e"s/<version>$oldVersion<\/version>/<version>$newVersion<\/version>/"  pom.xml model/pom.xml shared/pom.xml server/pom.xml api/pom.xml client/pom.xml war/pom.xml help/pom.xml
 
 sed -i -e"s/-$oldVersion/-$newVersion/g" .classpath
 sed -i -e"s/$oldVersion\//$newVersion\//g" .classpath
