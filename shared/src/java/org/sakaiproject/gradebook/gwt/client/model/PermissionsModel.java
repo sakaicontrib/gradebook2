00885880104d230e659819f7d049ee980ea2c66e
James Renfro <jlrenfro@ucdavid.edu>
2010-01-26 01:23:35 +0000

    GRBK-450
    Renamed PermissionModel

--- shared/src/java/org/sakaiproject/gradebook/gwt/client/model/PermissionsModel.java
+++ shared/src/java/org/sakaiproject/gradebook/gwt/client/model/PermissionsModel.java
@@ -1,6 +1,6 @@
 /**********************************************************************************
  *
- * $Id$
+ * $Id: PermissionEntryModel.java 63685 2009-09-30 01:33:01Z jlrenfro@ucdavis.edu $
  *
  ***********************************************************************************
  *
@@ -24,86 +24,84 @@
 package org.sakaiproject.gradebook.gwt.client.model;
 
 
-public class PermissionEntryModel extends EntityModel {
+public class PermissionsModel extends EntityModel {
 	
 	private static final long serialVersionUID = 1L;
 
-	public enum Key {ID, USER_ID, USER_DISPLAY_NAME, PERMISSION_ID, CATEGORY_ID, CATEGORY_DISPLAY_NAME, SECTION_ID, SECTION_DISPLAY_NAME, DELETE_ACTION };
-	
-	public PermissionEntryModel() {
+	public PermissionsModel() {
 		super();
 	}
 
 	public void setId(Long id) {
-		set(Key.ID.name(), id);
+		set(PermissionKey.ID.name(), id);
 	}
 	
 	public Long getId() {
-		return get(Key.ID.name());
+		return get(PermissionKey.ID.name());
 	}
 	
 	public void setUserId(String userId) {
-		set(Key.USER_ID.name(), userId);
+		set(PermissionKey.USER_ID.name(), userId);
 	}
 	
 	public String getUserId() {
-		return get(Key.USER_ID.name());
+		return get(PermissionKey.USER_ID.name());
 	}
 	
 	public void setUserDisplayName(String userDisplayName) {
-		set(Key.USER_DISPLAY_NAME.name(), userDisplayName);
+		set(PermissionKey.USER_DISPLAY_NAME.name(), userDisplayName);
 	}
 	
 	public String getUserDisplayName() {
-		return get(Key.USER_DISPLAY_NAME.name());
+		return get(PermissionKey.USER_DISPLAY_NAME.name());
 	}
 	
 	public void setPermissionId(String permissionId) {
-		set(Key.PERMISSION_ID.name(), permissionId);
+		set(PermissionKey.PERMISSION_ID.name(), permissionId);
 	}
 	
 	public String getPermissionId() {
-		return get(Key.PERMISSION_ID.name());
+		return get(PermissionKey.PERMISSION_ID.name());
 	}
 	
 	public void setCategoryId(Long categoryId) {
-		set(Key.CATEGORY_ID.name(), categoryId);
+		set(PermissionKey.CATEGORY_ID.name(), categoryId);
 	}
 	
 	public Long getCategoryId() {
-		return get(Key.CATEGORY_ID.name());
+		return get(PermissionKey.CATEGORY_ID.name());
 	}
 	
 	public void setCategoryDisplayName(String categoryDisplayName) {
-		set(Key.CATEGORY_DISPLAY_NAME.name(), categoryDisplayName);
+		set(PermissionKey.CATEGORY_DISPLAY_NAME.name(), categoryDisplayName);
 	}
 	
 	public String getCategoryDisplayName() {
-		return get(Key.CATEGORY_DISPLAY_NAME.name());
+		return get(PermissionKey.CATEGORY_DISPLAY_NAME.name());
 	}
 	
 	public void setSectionId(String sectionId) {
-		set(Key.SECTION_ID.name(), sectionId);
+		set(PermissionKey.SECTION_ID.name(), sectionId);
 	}
 	
 	public String getSectionId() {
-		return get(Key.SECTION_ID.name());
+		return get(PermissionKey.SECTION_ID.name());
 	}
 	
 	public void setSectionDisplayName(String sectionDisplayName) {
-		set(Key.SECTION_DISPLAY_NAME.name(), sectionDisplayName);
+		set(PermissionKey.SECTION_DISPLAY_NAME.name(), sectionDisplayName);
 	}
 	
 	public String getSectionDisplayName() {
-		return get(Key.SECTION_DISPLAY_NAME.name());
+		return get(PermissionKey.SECTION_DISPLAY_NAME.name());
 	}
 	
 	public void setDeleteAction(String deleteAction) {
-		set(Key.DELETE_ACTION.name(), deleteAction);
+		set(PermissionKey.DELETE_ACTION.name(), deleteAction);
 	}
 	
 	public String getDeleteAction() {
-		return get(Key.DELETE_ACTION.name());
+		return get(PermissionKey.DELETE_ACTION.name());
 	}
 	
 	@Override
