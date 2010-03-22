ca12a902fd50d701ac8564303352726e13e53e2a
James Renfro <jlrenfro@ucdavid.edu>
2010-03-22 17:33:44 +0000

    GRBK-546
    Fixes issue with GWT compilation by reintroducing the gradebook.tar.gz file.
    
    M:1.2.x-test
    M:1.2.x-prod
    M:1.2.0-rc2

--- server/src/java/org/sakaiproject/gradebook/gwt/server/WebAppToolServlet.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/server/WebAppToolServlet.java
@@ -37,6 +37,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.gradebook.gwt.client.AppConstants;
 import org.sakaiproject.tool.api.Tool;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 // This code was contributed by CARET to Sakai, it' modified below
 // for use in Gradebook2
@@ -44,6 +50,8 @@ public class WebAppToolServlet extends HttpServlet {
 
 	private static final Log log = LogFactory.getLog(WebAppToolServlet.class);
 
+	private static String version = null;
+	
 	/**
 	 * This init parameter should contain an url to the welcome page
 	 */
@@ -99,7 +107,35 @@ public class WebAppToolServlet extends HttpServlet {
 
 
 	private void addVersionAsCookie(HttpServletResponse response, String uri) {
+		if (version == null) {
+			readVersionFromFile();
+		}
+		if (version != null) {
+			Cookie cookie = new Cookie(AppConstants.VERSION_COOKIE_NAME, version);
+			cookie.setMaxAge(-1);
+			cookie.setPath(uri);
+			response.addCookie(cookie);
+		}
+	}
+	
+	private synchronized void readVersionFromFile() {
 		try {
+			if (version != null)
+				return;
+			
+			InputStream inputStream = this.getClass().getResourceAsStream("version.xml");
+			
+			if (inputStream != null) {
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+				
+				SvnInfoParserHelper helper = new SvnInfoParserHelper();
+				XMLReader parser = XMLReaderFactory.createXMLReader();
+				parser.setContentHandler(helper);
+				parser.parse(new InputSource(reader));
+				WebAppToolServlet.version = helper.getVersion();
+			}
+			
+			/*
 			InputStream inputStream = this.getClass().getResourceAsStream("version.txt");
 			if (inputStream != null) {
 				StringBuilder builder = new StringBuilder();
@@ -121,19 +157,45 @@ public class WebAppToolServlet extends HttpServlet {
 						String version = tokens[tokens.length - 2];
 						if (log.isDebugEnabled())
 							log.debug("Version is: " + version);
-						Cookie cookie = new Cookie(AppConstants.VERSION_COOKIE_NAME, version);
-						cookie.setMaxAge(-1);
-						cookie.setPath(uri);
-						response.addCookie(cookie);
+						
 					}
 
 				}
 				inputStream.close();
-			}
+			}*/
 
 		} catch (Exception e) {
 			log.warn("Unable to read version file", e);
 		} 
+	}
+	
+	private class SvnInfoParserHelper extends DefaultHandler {
+		
+		private boolean isReading = false;
+		private String url;
+
+		public void startElement(String nsURI, String strippedName,
+						String tagName, Attributes attributes) throws SAXException {
+			if (tagName.equalsIgnoreCase("url"))
+				isReading = true;
+		}
+
+		public void characters(char[] ch, int start, int length) {
+			if (isReading) {
+				url = new String(ch, start, length);
+				isReading = false;
+			}
+		}
+
+		public String getVersion() {
+			if (url != null && !url.isEmpty()) {
+				String[] tokens = url.split("/");
+				if (tokens.length > 2) {
+					return tokens[tokens.length - 2];
+				}
+			}
+			return null;
+		}
 	}
 
 }
