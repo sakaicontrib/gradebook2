d7b18294fde90f0d86eb49d4679662042f0ed9c6
Thomas Amsler <tpamsler@ucdavis.edu>
2010-06-22 16:45:56 +0000

    GRBK-369 : adding log statement that shows the query parameters for loading the stats grid.

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/hibernate/GradebookToolServiceImpl.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/hibernate/GradebookToolServiceImpl.java
@@ -801,8 +801,12 @@ public class GradebookToolServiceImpl extends HibernateDaoSupport implements Gra
 	// GRBK-40 : TPA : Eliminated the in java filtering
 	public List<AssignmentGradeRecord> getAllAssignmentGradeRecords(final Long gradebookId, final String[] realmIds, final String[] roleNames) {
 		HibernateCallback hc = new HibernateCallback() {
+			
 			public Object doInHibernate(Session session) throws HibernateException {
 
+				// FIXME: this log.info needs to be removed
+				log.info("DEBUG: #### : realmId = " + ((null != realmIds && realmIds.length > 0) ? realmIds[0] : "realmIds is null or zero size"));
+				
 				Query query = null;
 
 				StringBuilder builder = new StringBuilder();
