151460b9e55a15ccd3cb5e5585c043e38cf6de51
Thomas Amsler <tpamsler@ucdavis.edu>
2011-01-21 23:18:04 +0000

    GRBK-805

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradeRecordCalculationUnit.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradeRecordCalculationUnit.java
@@ -37,42 +37,112 @@ public interface GradeRecordCalculationUnit {
 	 */
 	public BigDecimal calculateEqually(int numItems);
 
-	// FIXME (GRBK-795)- Further Invesigation Required - The impl sets the percentage score to zero if we're in a divide by zero situation, that seems strange... 
+	// FIXME (GRBK-795)- Further Investigation Required - The impl sets the percentage score to zero if we're in a divide by zero situation, that seems strange... 
 
 	/**
 	 * Calculates the percentage score by taking the points received divided by the points possible. 
 	 * If either points possible or points received are null, then nothing happens. 
 	 * 
-	 * If either points possible or points recieved are zero, then the percentage score is set to zero. 
+	 * If either points possible or points received are zero, then the percentage score is set to zero. 
 	 */
 	public void calculatePercentageScore();
 	
+	/**
+	 * If pointsReceived and pointsPossible are not null, it subtracts pointsReceived from pointsPossible
+	 * and assigns the result to pointsDifference. If either of pointsReceived or pointsPossible are null,
+	 * pointsDifference set to null.
+	 */
 	public void calculateRawDifference();
 	
+	/**
+	 * Getter method
+	 * 
+	 * @return percentOfCategory
+	 */
 	public BigDecimal getPercentOfCategory();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return scaledScore
+	 */
 	public BigDecimal getScaledScore();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return isExcused
+	 */
 	public boolean isExcused();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return isExtraCredit
+	 */
 	public boolean isExtraCredit();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return pointsReceived
+	 */
 	public BigDecimal getPointsReceived();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return pointsPossible
+	 */
 	public BigDecimal getPointsPossible();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return percentageScore
+	 */
 	public BigDecimal getPercentageScore();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param isExcused
+	 */
 	public void setExcused(boolean isExcused);
 
+	/**
+	 * Getter method
+	 * 
+	 * @return isDropped
+	 */
 	public boolean isDropped();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param isDropped
+	 */
 	public void setDropped(boolean isDropped);
 
+	/**
+	 * Getter method
+	 * 
+	 * @return actualRecord
+	 */
 	public Object getActualRecord();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param actualRecord
+	 */
 	public void setActualRecord(Object actualRecord);
 
+	/**
+	 * Getter method
+	 * 
+	 * @return pointsDifference
+	 */
 	public BigDecimal getPointsDifference();
 
 }
