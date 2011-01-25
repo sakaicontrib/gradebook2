f73e809d8ef33c8df758449870e4a38b0181e100
Thomas Amsler <tpamsler@ucdavis.edu>
2011-01-25 00:59:37 +0000

    GRBK-805

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradebookCalculationUnit.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradebookCalculationUnit.java
@@ -6,14 +6,44 @@ import java.util.Map;
 
 import org.sakaiproject.gradebook.gwt.sakai.calculations.GradeRecordCalculationUnit;
 
+/**
+ * 
+ * This class performs top level gradebook calculations.
+ *
+ */
 
 public interface GradebookCalculationUnit {
 
+	/**
+	 * This method:
+	 * - Sums up all the points received, points possible and and extra credit points.
+	 * - Calculates the percentage score by dividing points received by points possible.
+	 * - When appropriate and if present, it add the scaled extra credit points to the percentage score.
+	 * - At the end, it returns the course grade by multiplying the percentage score by 100
+	 * 
+	 * @param units
+	 * @param totalGradebookPoints
+	 * @param isExtraCreditScaled
+	 * @return ZERO if points possible is ZERO, or if points received is ZERO otherwise it returns the course grade
+	 */
 	public BigDecimal calculatePointsBasedCourseGrade(List<GradeRecordCalculationUnit> units, BigDecimal totalGradebookPoints, boolean isExtraCreditScaled);
 	
+	/**
+	 * 
+	 * @param categoryGradeUnitListMap
+	 * @param totalGradebookPoints
+	 * @param isExtraCreditScaled
+	 * @return
+	 */
 	public BigDecimal calculatePointsBasedCourseGrade(Map<String, List<GradeRecordCalculationUnit>> categoryGradeUnitListMap, BigDecimal totalGradebookPoints, boolean isExtraCreditScaled);
 	
+	/**
+	 * 
+	 * @param categoryGradeUnitListMap
+	 * @param totalGradebookPoints
+	 * @param isExtraCreditScaled
+	 * @return
+	 */
 	public BigDecimal calculateWeightedCourseGrade(Map<String, List<GradeRecordCalculationUnit>> categoryGradeUnitListMap, BigDecimal totalGradebookPoints, boolean isExtraCreditScaled);
-	
 	
 }
