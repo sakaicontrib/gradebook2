7ed5668f7153bcb8f770dd8ca4e84e094c4261c1
Thomas Amsler <tpamsler@ucdavis.edu>
2011-01-25 00:24:48 +0000

    GRBK-805

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/CategoryCalculationUnit.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/CategoryCalculationUnit.java
@@ -3,31 +3,104 @@ package org.sakaiproject.gradebook.gwt.sakai.calculations;
 import java.math.BigDecimal;
 import java.util.List;
 
+/**
+ * 
+ * This class performs category level calculations as well as acting as a POJO, storing category level data.
+ *
+ */
 public interface CategoryCalculationUnit {
 
+	/**
+	 * This method sums up all the scaled scores using different processes paths to handle
+	 * drop lowest and extra credit
+	 * 
+	 * @param units in the category
+	 * @param isExtraCreditScaled
+	 * @return the sum of all relevant scaled scores
+	 */
 	public BigDecimal calculate(List<GradeRecordCalculationUnit> units, boolean isExtraCreditScaled);
 	
+	/**
+	 * Getter method
+	 * 
+	 * @return isExtraCredit
+	 */
 	public boolean isExtraCredit();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return categoryWeightTotal
+	 */
 	public BigDecimal getCategoryWeightTotal();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return categoryGrade
+	 */
 	public BigDecimal getCategoryGrade();
 
+	/**
+	 * Getter method
+	 * 
+	 * @return dropLowest
+	 */
 	public int getDropLowest();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param dropLowest
+	 */
 	public void setDropLowest(int dropLowest);
 
+	/**
+	 * Getter method
+	 * 
+	 * @return isPointsWeighted
+	 */
 	public boolean isPointsWeighted();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param isPointsWeighted
+	 */
 	public void setPointsWeighted(boolean isPointsWeighted);
 
+	/**
+	 * Setter method
+	 * 
+	 * @param categoryGrade
+	 */
 	public void setCategoryGrade(BigDecimal categoryGrade);
 
+	/**
+	 * Getter method
+	 * 
+	 * @return totalCategoryPoints
+	 */
 	public BigDecimal getTotalCategoryPoints();
 
+	/**
+	 * Setter method
+	 * 
+	 * @param totalCategoryPoints
+	 */
 	public void setTotalCategoryPoints(BigDecimal totalCategoryPoints);
 	
-	public int getTotalNumberOfItems(); 
+	/**
+	 * Getter method
+	 * 
+	 * @return totalNumberOfItems
+	 */
+	public int getTotalNumberOfItems();
 	
+	/**
+	 * Setter method
+	 * 
+	 * @param totalNumberOfItems
+	 */
 	public void setTotalNumberOfItems(int totalNumberOfItems);
 }
