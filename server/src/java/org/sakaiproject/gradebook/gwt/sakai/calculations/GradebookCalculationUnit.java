0907d685a750bcdf7429fe17ef177ea1ff269555
Thomas Amsler <tpamsler@ucdavis.edu>
2011-01-26 01:05:51 +0000

    GRBK-805

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradebookCalculationUnit.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/calculations/GradebookCalculationUnit.java
@@ -8,7 +8,7 @@ import org.sakaiproject.gradebook.gwt.sakai.calculations.GradeRecordCalculationU
 
 /**
  * 
- * This class performs top level gradebook calculations.
+ * This class performs top level gradebook calculations. It also keeps track of the associated CategoryCalculationUnits.
  *
  */
 
@@ -16,7 +16,8 @@ public interface GradebookCalculationUnit {
 
 	/**
 	 * This method:
-	 * - Sums up all the points received, points possible and and extra credit points.
+	 * - Sums up all the points received, points possible and extra credit points.
+	 * - If the sum of points received or the sum of points possible is zero, we return zero as the course grade
 	 * - Calculates the percentage score by dividing points received by points possible.
 	 * - When appropriate and if present, it add the scaled extra credit points to the percentage score.
 	 * - At the end, it returns the course grade by multiplying the percentage score by 100
@@ -29,16 +30,24 @@ public interface GradebookCalculationUnit {
 	public BigDecimal calculatePointsBasedCourseGrade(List<GradeRecordCalculationUnit> units, BigDecimal totalGradebookPoints, boolean isExtraCreditScaled);
 	
 	/**
+	 * This method:
+	 * - Sums up all the points received, points possible, and extra credit points per CATEGORY
+	 * - If the sum of points received or the sum of points possible is zero, we return zero as the course grade
+	 * - Calculates the percentage score by dividing points received by points possible.
+	 * - When appropriate and if present, it add the scaled extra credit points to the percentage score.
+	 * - At the end, it returns the course grade by multiplying the percentage score by 100
 	 * 
 	 * @param categoryGradeUnitListMap
 	 * @param totalGradebookPoints
 	 * @param isExtraCreditScaled
-	 * @return
+	 * @return ZERO if points possible is ZERO, or if points received is ZERO otherwise it returns the course grade
 	 */
 	public BigDecimal calculatePointsBasedCourseGrade(Map<String, List<GradeRecordCalculationUnit>> categoryGradeUnitListMap, BigDecimal totalGradebookPoints, boolean isExtraCreditScaled);
 	
 	/**
-	 * 
+	 * This method:
+	 * - Sums up all the points received, points possible, and extra credit points per CATEGORY 
+	 * - ...
 	 * @param categoryGradeUnitListMap
 	 * @param totalGradebookPoints
 	 * @param isExtraCreditScaled
