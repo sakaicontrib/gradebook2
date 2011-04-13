a2ab3c64333533826d6b03b68745c116e3074e5b
Thomas Amsler <tpamsler@ucdavis.edu>
2011-04-13 05:46:31 +0000

    GRBK-710

--- server/src/java/org/sakaiproject/gradebook/gwt/server/WebAppToolServlet.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/server/WebAppToolServlet.java
@@ -37,12 +37,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.sakaiproject.gradebook.gwt.client.AppConstants;
 import org.sakaiproject.tool.api.Tool;
-import org.xml.sax.Attributes;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 // This code was contributed by CARET to Sakai, it' modified below
 // for use in Gradebook2
@@ -61,7 +55,7 @@ public class WebAppToolServlet extends HttpServlet {
 
 	protected void service(final HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-
+		
 		final String contextPath = request.getContextPath();
 		request.setAttribute(Tool.NATIVE_URL, Tool.NATIVE_URL);
 		HttpServletRequest wrappedRequest = new HttpServletRequestWrapper(
@@ -125,50 +119,16 @@ public class WebAppToolServlet extends HttpServlet {
 			if (version != null)
 				return;
 			
-			InputStream inputStream = this.getClass().getResourceAsStream("version.xml");
+			InputStream inputStream = this.getClass().getResourceAsStream("VERSION.txt");
 			
 			if (inputStream != null) {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-				
-				SvnInfoParserHelper helper = new SvnInfoParserHelper();
-				XMLReader parser = XMLReaderFactory.createXMLReader();
-				parser.setContentHandler(helper);
-				parser.parse(new InputSource(reader));
-				WebAppToolServlet.version = helper.getVersion();
+				WebAppToolServlet.version = reader.readLine();
+				log.info("Setting Gradebook2 version = " + WebAppToolServlet.version);
 			}
 			
 		} catch (Exception e) {
 			log.warn("Unable to read version file", e);
 		} 
 	}
-	
-	private static class SvnInfoParserHelper extends DefaultHandler {
-		
-		private boolean isReading = false;
-		private String url;
-
-		public void startElement(String nsURI, String strippedName,
-						String tagName, Attributes attributes) throws SAXException {
-			if (tagName.equalsIgnoreCase("url"))
-				isReading = true;
-		}
-
-		public void characters(char[] ch, int start, int length) {
-			if (isReading) {
-				url = new String(ch, start, length);
-				isReading = false;
-			}
-		}
-
-		public String getVersion() {
-			if (url != null && !url.equals("")) {
-				String[] tokens = url.split("/");
-				if (tokens.length > 1) {
-					return tokens[tokens.length - 1];
-				}
-			}
-			return null;
-		}
-	}
-
 }
