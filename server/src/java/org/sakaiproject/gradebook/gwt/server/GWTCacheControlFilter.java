466a4bc23dcef86d72fed916e372fb5fda136f9e
Thomas Amsler <tpamsler@ucdavis.edu>
2011-04-29 05:51:53 +0000

    GRBK-718 : experimenting with setting cachee directive.

--- server/src/java/org/sakaiproject/gradebook/gwt/server/GWTCacheControlFilter.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/server/GWTCacheControlFilter.java
@@ -34,11 +34,15 @@ public class GWTCacheControlFilter implements Filter {
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
 
+		CacheResponseWrapper cacheResponseWrapper = null;
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String requestURI = httpRequest.getRequestURI();
 
 		if (requestURI.contains(".nocache.")) {
 
+			cacheResponseWrapper = new CacheResponseWrapper((HttpServletResponse)response);
+			filterChain.doFilter(request, cacheResponseWrapper);
+			
 			Date now = new Date();
 			HttpServletResponse httpResponse = (HttpServletResponse) response;
 			httpResponse.setDateHeader("Date", now.getTime());
@@ -47,8 +51,10 @@ public class GWTCacheControlFilter implements Filter {
 			httpResponse.setHeader("Pragma", "no-cache");
 			httpResponse.setHeader("Cache-control", "no-cache, no-store, must-revalidate");
 		}
-
-		filterChain.doFilter(request, response);
+		else {
+			
+			filterChain.doFilter(request, response);
+		}
 	}
 }
 
