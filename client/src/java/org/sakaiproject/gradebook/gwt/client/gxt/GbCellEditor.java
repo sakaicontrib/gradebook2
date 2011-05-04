a406ff40afcc8dfbeee94f53cff16ecc438f09da
Michael Went <mjwenk@ucdavis.edu>
2011-05-04 21:21:45 +0000

    GRBK-950 / GRBK-900 See JIRA

--- client/src/java/org/sakaiproject/gradebook/gwt/client/gxt/GbCellEditor.java
+++ client/src/java/org/sakaiproject/gradebook/gwt/client/gxt/GbCellEditor.java
@@ -12,6 +12,21 @@ public class GbCellEditor extends CellEditor {
 	public GbCellEditor(Field<? extends Object> field) {
 		super(field);
 	}
+	
+	/*
+	 * GRBK-900 
+	 * We basically don't allow our editor to blur.  We do this because if we do allow it to blur it will cause
+	 * fields to lose focus when we don't want them to do so.  
+	 * 
+	 * See: http://www.sencha.com/forum/showthread.php?132296-Grid-KEY_ENTER-handling-in-onEditorKey%28%29-doesn-t-the-cell-edit-anymore
+	 * (non-Javadoc)
+	 * @see com.extjs.gxt.ui.client.widget.Editor#triggerBlur(com.extjs.gxt.ui.client.widget.form.TriggerField)
+	 */
+
+	@Override
+	protected void triggerBlur(TriggerField field) {
+		return; 
+	}
 
 	/*
 	protected void onSpecialKey(FieldEvent fe) {
