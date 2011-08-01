feeb81c35473e86f833b7a38207d3c65c2c1c417
Thomas Amsler <tpamsler@ucdavis.edu>
2011-08-01 16:21:47 +0000

    GRBK-1064 : Merging patch into trunk.

--- client/src/java/org/sakaiproject/gradebook/gwt/client/gxt/JsonUtil.java
+++ client/src/java/org/sakaiproject/gradebook/gwt/client/gxt/JsonUtil.java
@@ -2,23 +2,16 @@ package org.sakaiproject.gradebook.gwt.client.gxt;
 
 import org.sakaiproject.gradebook.gwt.client.gxt.model.EntityOverlay;
 
-import com.google.gwt.core.client.JsArray;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
 
 public class JsonUtil {
 
 	
 	public static EntityOverlay toOverlay(String json) {
-		JsArray<EntityOverlay> array = asOverlay(json);
 		
-		if (array != null && array.length() > 0) {
-			return array.get(0);
-		}
-		
-		return null;
+		 JSONObject  jsonObject = (JSONObject) JSONParser.parseStrict(json);
+		 return  jsonObject.getJavaScriptObject().cast();
 	}
-	
-	public static final native JsArray<EntityOverlay> asOverlay(String json) /*-{
-		return eval("[" + json + "]");
-	}-*/;
 	
 }
