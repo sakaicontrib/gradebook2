5b9d12a620c59f8464f93a184a3a5b0493471a66
Michael Went <mjwenk@ucdavis.edu>
2011-10-20 18:21:14 +0000

    GRBK-853 - Letter grades should not promote a "0" in final grade submission

--- server/src/java/org/sakaiproject/gradebook/gwt/sakai/rest/resource/SubmitFinalGrades.java
+++ server/src/java/org/sakaiproject/gradebook/gwt/sakai/rest/resource/SubmitFinalGrades.java
@@ -13,9 +13,11 @@ import javax.ws.rs.core.Context;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.sakaiproject.gradebook.gwt.client.model.Gradebook;
 import org.sakaiproject.gradebook.gwt.client.model.Learner;
 import org.sakaiproject.gradebook.gwt.client.model.Roster;
 import org.sakaiproject.gradebook.gwt.client.model.key.LearnerKey;
+import org.sakaiproject.gradebook.gwt.client.model.type.GradeType;
 import org.sakaiproject.gradebook.gwt.sakai.InstitutionalAdvisor.Column;
 
 @Path("submit")
@@ -44,6 +46,19 @@ public class SubmitFinalGrades extends Resource {
 			e.printStackTrace();
 			return null;
 		}
+		
+		
+		/*
+		 * GRBK-853
+		 * 853 says that for final grade submission we submit an 'F' if the grade is '0' per the grading scale.  Since '0' is a valid grade 
+		 * per the hardcoded scale, but is not displayed to the user. 
+		 * 
+		 */
+
+		Gradebook gb = service.getGradebook(gradebookUid);
+
+		boolean isLetterGradeGB = gb.getGradebookItemModel().getGradeType() == GradeType.LETTERS;
+
 
 		List<Map<Column,String>> studentDataList = new ArrayList<Map<Column,String>>();
 
@@ -56,7 +71,14 @@ public class SubmitFinalGrades extends Resource {
 				studentData.put(Column.EXPORT_USER_ID, (String)studentModel.get(LearnerKey.S_EXPRT_USR_ID.name()));
 				studentData.put(Column.STUDENT_NAME, (String)studentModel.get(LearnerKey.S_DSPLY_NM.name()));
 				studentData.put(Column.EXPORT_CM_ID, (String)studentModel.get(LearnerKey.S_EXPRT_CM_ID.name()));
-				studentData.put(Column.LETTER_GRADE, (String)studentModel.get(LearnerKey.S_LTR_GRD.name()));
+				// GRBK-853 - Just in time switch the letter grade.  
+				String letterGrade = (String)studentModel.get(LearnerKey.S_LTR_GRD.name());
+				if (isLetterGradeGB && letterGrade != null && !"".equals(letterGrade) && letterGrade.equals("0"))
+				{
+					letterGrade = "F"; 
+				}
+				studentData.put(Column.LETTER_GRADE, letterGrade);
+
 				studentData.put(Column.RAW_GRADE, (String)studentModel.get(LearnerKey.S_RAW_GRD.name()));
 				studentDataList.add(studentData);
 			}
